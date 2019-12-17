ae20bf2883856aa77baf774ff0b59a0c7f0360c0
Balazs-D <balazs.danyadi@gmail.com>
2019-12-17 15:33:36 +0100

    new style

--- src/components/App.js
+++ src/components/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>TO DO:</h1>
+        <h1>TO DO</h1>
         <Todo />
       </header>
     </div>
