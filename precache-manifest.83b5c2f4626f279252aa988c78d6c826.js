ec3052c5d1c8c44f2558aa43ada01d0c47e48a19
Balazs-D <balazs.danyadi@gmail.com>
2019-12-17 16:16:36 +0100

    Updates

---
+++ precache-manifest.83b5c2f4626f279252aa988c78d6c826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9159cec7b6f6036f9c8ef7dbcd2c750d",
+    "url": "/ToDoApp_React/index.html"
+  },
+  {
+    "revision": "c3ee265aba7681b2e0f1",
+    "url": "/ToDoApp_React/static/css/main.a9d70f40.chunk.css"
+  },
+  {
+    "revision": "5038b43692885f7a6569",
+    "url": "/ToDoApp_React/static/js/2.0aa9bd2a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ToDoApp_React/static/js/2.0aa9bd2a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c3ee265aba7681b2e0f1",
+    "url": "/ToDoApp_React/static/js/main.fba7563f.chunk.js"
+  },
+  {
+    "revision": "17ae7cc19b6d3dd7eca5",
+    "url": "/ToDoApp_React/static/js/runtime-main.787c0994.js"
+  }
+]);
